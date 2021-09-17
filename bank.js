aa577888962ca72b6271f949ca4918c47c35d706
madhushree781 <88770288+madhushree781@users.noreply.github.com>
2021-09-17 15:19:29 +0530

    Add files via upload

---
+++ bank.js
@@ -0,0 +1,12 @@
+var bankAccount;
+var deposit;
+prompt("enter amt for bankAccount")
+prompt("enter amt to deposit")
+alert("deposit success")
+var total=bankAccount+deposit;
+alert("total "+total)
+
+alert("Welcome to your bank's website!")
+var deposit = prompt("How much would you like to deposit today?")
+alert("You've deposited: "+deposit+" into your account.")
+console.log("You are a cool person!")
---
+++ kglbs.js
@@ -0,0 +1,6 @@
+var kg;
+var lbs;
+var kg=lbs*0.454;
+prompt("enter kg value")
+prompt("enter lbs value")
+alert("parseint(kg)")
